f41a99b2487c53c27718c84d773f09d4839b8de2
Radvil <radvil@access-mobile.com>
2022-05-26 18:15:50 +0800

    Rework project structures

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'instangular',
+  displayName: 'web',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/instangular',
+  coverageDirectory: '../../coverage/apps/web',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
--- apps/web/src/app/nx-welcome.component.ts
+++ apps/web/src/app/nx-welcome.component.ts
@@ -433,7 +433,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome live-chat 👋
+            Welcome web 👋
           </h1>
         </div>
 
